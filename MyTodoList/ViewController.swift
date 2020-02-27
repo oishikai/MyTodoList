36ec29586112d1b195eb9569cd82a9c63345a78e
Kai Oishi <bokuchan.kai@gmail.com>
2020-02-27 18:33:25 +0900

    first commit

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by kai on 2020/02/27.
+//  Copyright © 2020 kai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
