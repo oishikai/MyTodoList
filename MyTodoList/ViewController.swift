69853edef3b8d430db287d1c8317430ed606beb9
Kai Oishi <bokuchan.kai@gmail.com>
2020-02-27 20:50:55 +0900

    p253 commit

--- MyTodoList/ViewController.swift
+++ MyTodoList/ViewController.swift
@@ -55,6 +55,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         
     }
+    // テーブルの行数を返却する
+    func tableView(_ tableView: UITableView,numberOfRowsInSection section: Int) -> Int{
+        return todoList.count
+    }
     
+    //テーブルの行ごとのセルを返却する
+    func tableView(_ tableView: UITableView,cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        //storyboardで指定したtodoCell識別子を利用して再利用可能なセルを取得する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell",for: indexPath)
+        //行番号にあったToDoのタイトルを取得
+        let todoTitle = todoList[indexPath.row]
+        //セルのラベルにToDoのタイトルをセット
+        cell.textLabel?.text = todoTitle
+        return cell
+    }
 }
 
